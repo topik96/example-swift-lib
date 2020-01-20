8e24d8482b8e3b5d4a28df3d943966781db9d54c
topik <topikmujianto7@gmail.com>
2020-01-20 14:20:36 +0700

    update unit test & app

---
+++ ExampleSwiftLibApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleSwiftLibApp
+//
+//  Created by topik mujianto on 20/01/20.
+//  Copyright © 2020 topik mujianto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
