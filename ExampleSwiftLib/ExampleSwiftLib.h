68121a1c82b73290971013cb258f6974d29d8e4a
topik <topikmujianto7@gmail.com>
2020-01-20 14:16:07 +0700

    Initial Commit

---
+++ ExampleSwiftLib/ExampleSwiftLib.h
@@ -0,0 +1,19 @@
+//
+//  ExampleSwiftLib.h
+//  ExampleSwiftLib
+//
+//  Created by topik mujianto on 20/01/20.
+//  Copyright © 2020 topik mujianto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExampleSwiftLib.
+FOUNDATION_EXPORT double ExampleSwiftLibVersionNumber;
+
+//! Project version string for ExampleSwiftLib.
+FOUNDATION_EXPORT const unsigned char ExampleSwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleSwiftLib/PublicHeader.h>
+
+
